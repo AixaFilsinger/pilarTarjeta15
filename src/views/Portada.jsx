76171e7503f4192c2a9504bc0308066728dca1c8
Aixa Filsinger <macafil99@gmail.com>
2025-10-07 15:45:42 -0300

    se centro los elementos

--- src/views/Portada.jsx
+++ src/views/Portada.jsx
@@ -2,7 +2,7 @@ import quince from "../assets/15.png"
 
 const Portada = () => {
     return (
-        <section className="text-center portada">
+        <section className="d-flex flex-column align-items-center portada">
             <aside className="linea">
               <h2 className="mis">mis</h2>
               <img src={quince} alt="" width={150} />
